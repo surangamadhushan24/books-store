daba9f9edffd07ce750c247a1561f74a2361cc07
suranga madhushan <150942338+surangamadhushan24@users.noreply.github.com>
2026-02-24 21:34:26 +0530

    create book entity

---
+++ src/main/java/com/bookstore/entity/Book.java
@@ -0,0 +1,34 @@
+package com.bookstore.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String title;
+    @Column(nullable = false)
+    private String author;
+    @Column(nullable = false,unique = true)
+    private String description;
+    @Column(nullable = false)
+    private String genre;
+
+}
+
